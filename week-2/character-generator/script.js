83641a71f11817f550a99349417323966e7af8e1
Alex Renard <prioxisfx@gmail.com>
2024-03-28 22:10:15 -0500

    Did assignment 2-2

--- week-2/character-generator/script.js
+++ week-2/character-generator/script.js
@@ -3,23 +3,53 @@
   Chapter 1
   Programming Assignment
 
-  Author:
-  Date:
-  Filename:
+  Author: William Renard
+  Date: 3/28/2024
+  Filename: script.js
 */
 
 "use strict";
 
 function createCharacter(name, gender, characterClass) {
-  // TODO: Implement this function
+
+  return {
+    getName: function() {
+      return name;
+    },
+    getGender: function() {
+      return gender;
+    },
+    getClass: function() {
+      return characterClass;
+    }
+  }
 }
 
 document.getElementById("generateHero").addEventListener("click", function(e) {
   e.preventDefault();
 
-  // TODO: Get form values
+  // Got Form Values
+  let nameField = document.getElementById("heroName");
+  let genderSelect = document.getElementById("heroGender");
+  let classSelect = document.getElementById("heroClass");
 
-  // TODO: Create character
+    // get the label elements for the character information
+    let nameLabel = document.getElementById("nameLabel");
+    let classLabel = document.getElementById("classLabel");
+    let genderLabel = document.getElementById("genderLabel");
 
+  //Create the character
+  const character = createCharacter(nameField.value, genderSelect.value, classSelect.value);
+
+  nameLabel.textContent = character.getName();
+  genderLabel.textContent = character.getGender();
+  classLabel.textContent = character.getClass();
+
+  // hide the form, show the values of the newly created character.
+  document.getElementById("characterForm").classList.add("hidden");
+  document.getElementById("characterOutput").classList.add("visible");
+  document.getElementById("characterOutput").classList.remove("hidden");
   // TODO: Display character information
+  // think I might hide the create form and then show the character output div.
+
 });
