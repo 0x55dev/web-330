b443695fa0b1b3aae53f2a8b8a08776411f0bcd9
Alex Renard <prioxisfx@gmail.com>
2024-04-20 21:30:41 -0500

    did week 5 assignment

--- week-5/restaurant-reservation/script.js
+++ week-5/restaurant-reservation/script.js
@@ -3,24 +3,49 @@
   Chapter 2
   Programming Assignment
 
-  Author:
-  Date:
-  Filename:
+  Author: William Renard
+  Date: 4/20/2024
+  Filename: script.js
 */
 
 // Create an in-memory object array for each table in the restaurant
 let tables = [
-  // Add your table objects here
+  { tableNumber: 1, capacity: 5, isReserved: false},
+  { tableNumber: 2, capacity: 10, isReserved: false},
+  { tableNumber: 3, capacity: 2, isReserved: true},
+  { tableNumber: 4, capacity: 3, isReserved: true},
+  { tableNumber: 5, capacity: 4, isReserved: false},
 ];
 
 // Create a function reserveTable
 function reserveTable(tableNumber, callback, time) {
-  // Add your code here
+  for(let i = 0; i < tables.length; i++) { // loop through the tables
+    if(tables[i].tableNumber === tableNumber) {  // check each table until we find one that matches the supplied number
+      if(tables[i].isReserved) { // if the table is reserved then we're going to immediately call the callback and return out of the function.
+        callback("Table is already reserved.");
+        return; // return so we do not hit the callback twice
+      }
+      else {
+        tables[i].isReserved = true; // set table to reserved and start the timeout for the callback.
+        setTimeout(() => {
+          callback("Table is now reserved."); // announce that table is now reserved.
+        }, time);
+        return;
+      }
+    }
+  }
+  callback("No table with that number found."); // if we loop through everything and do not find the table, then we call the callback function
+}
+
+function reservationMessage(message)
+{
+  document.getElementById("message").innerHTML = message;
 }
 
 // When the form is submitted, call the reserveTable function
 document
   .getElementById("reservationForm")
   .addEventListener("submit", function (e) {
-    // Add your code here
+    e.preventDefault(); // form submission kept refreshing the page. This prevents that from happening
+    reserveTable(parseInt(document.getElementById("tableNumber").value), reservationMessage, 1000); // we need to suppy the table number, name does not necessarily matter since it is not being stored anywhere.
   });
